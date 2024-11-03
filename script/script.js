21b331ce20169e603c0620465aaa8de6620d784a
yuuKaze <151874490+yuuKazeen@users.noreply.github.com>
2024-11-03 22:48:55 +0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -7,7 +7,7 @@ const images = [
 function showNextImage() {
     currentIndex = (currentIndex + 1) % images.length;
     const imgElement = document.getElementById('carousel-image');
-    imgElement.src = images[currentIndex];
+    imgElement.src = "image/" + images[currentIndex];
 }
 
 setInterval(showNextImage, 3000); 
